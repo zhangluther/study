0cd80cbb38589cfabca33cc5326f46ef61d2ecc3
Zhang Wei <zhangluther@yahoo.com>
2017-07-26 13:59:20 +0800

    0.1.2

---
+++ ControllersAndActions/ControllersAndActions/Controllers/DerivedController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ControllersAndActions.Controllers
+{
+    public class DerivedController : Controller
+    {
+        // GET: Derived
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Hello from the derivedcontroller index method";
+            DateTime datenow = DateTime.Now;
+            return View("MyView");
+        }
+    }
+}
