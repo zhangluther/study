07446d10d12d0ac0d7e94c541226b75c2992e5e4
Zhang Wei <zhangluther@yahoo.com>
2017-07-26 23:30:03 +0800

    0.3.1

---
+++ Views/Views/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Views.Models;
+
+namespace Views.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            Person person = new Person
+            {
+                Surname = "Wei",
+                FirstName = "Zhang",
+                Patronymic = "China",
+                BirthDate = new DateTime(1988, 6, 15)
+            };
+            return View(person);
+        }
+
+        public ActionResult List()
+        {
+            return View();
+        }
+    }
+}
---
+++ Views/Views/Models/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Views.Models
+{
+    public class Person
+    {
+        public string Surname { get; set; }
+        public string FirstName { get; set; }
+        public string Patronymic { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
